ca11a8077dfe424a6f416395bb4f4b4fa093bcbd
KC Swanson <kcs7@rice.edu>
2025-12-22 21:09:52 -0600

    standardize env vars

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,17 +3,20 @@ import { Pool, QueryResultRow } from 'pg';
 // Configure DB connection
 
 const {
-  DB_HOST,
-  DB_NAME,
-  DB_USER,
-  DB_PASSWORD,
+  POSTGRES_HOST,
+  POSTGRES_DATABASE,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
 } = process.env
 
 const pool = new Pool({
-  host: DB_HOST,
-  database: DB_NAME,
-  user: DB_USER,
-  password: DB_PASSWORD,
+  host: POSTGRES_HOST,
+  database: POSTGRES_DATABASE,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export const query = <Result extends QueryResultRow>(text: string) => {
